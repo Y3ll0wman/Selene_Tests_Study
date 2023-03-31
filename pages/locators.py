eba40c9b28354510fbbf06a0589fe5c404756451
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-03-31 19:13:39 +0300

    Added locators.py and config.json

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+class LoginPageLocators():
+    input_login = ".login form div:nth-child(2) input"
+    input_password = ".login form div:nth-child(3) input"
+    button_submit = ".login form div:nth-child(5) button:nth-child(1)"
+
+
+class MainPageLocators():
+    header_wrapper = "#main-header"
+    sidebar_menu_payment_button = "#sidebarMenu div nav div:nth-child(5) a span:nth-child(2)"
